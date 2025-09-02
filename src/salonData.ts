a77c54d140e1a7b3c78a5ea560b1b83f68bb8044
osei224 <seikann0224@gmail.com>
2025-09-02 18:33:02 +0900

    Create salonData.ts

---
+++ src/salonData.ts
@@ -0,0 +1,8 @@
+// src/salonData.ts
+export const salon = {
+  name: "HAIR & SPA Lumière",
+  address: "大阪市〇〇区〇〇 1-2-3 ルミエールビル2F",
+  tel: "06-1234-5678",
+  hours: "10:00-20:00（最終受付19:00）",
+  mapUrl: "",
+};
