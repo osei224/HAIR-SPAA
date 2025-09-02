3f7863c0dadde718f7bd11ccf84c114cd4311dc9
osei224 <seikann0224@gmail.com>
2025-09-02 10:36:45 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
-// src/App.tsx
-import React from "react";
-import "./index.css";
+import Hero from "./components/Hero";
+import Features from "./components/Features";
+import Menu from "./components/Menu";
+import Pricing from "./components/Pricing";
+import Testimonials from "./components/Testimonials";
+import ReservationDialog from "./components/ReservationDialog";
 import Access from "./components/Access";
-
-const salonData = {
-  name: "HAIR & SPA Lumière",
-  address: "大阪市〇〇区〇〇 1-2-3 ルミエールビル2F",
-  mapUrl:
-    "https://www.google.com/maps/embed?pb=...（ここを実際の埋め込みURLに）",
-  phone: "06-1234-5678",
-  hours: "10:00-20:00（最終受付19:00）",
-};
+import Footer from "./components/Footer";
+import "./index.css";
 
 export default function App() {
+  const salonData = {
+    name: "HAIR & SPA Lumière",
+    address: "大阪市〇〇区〇〇 1-2-3 ルミエールビル2F",
+    tel: "06-1234-5678",
+    hours: "10:00-20:00（最終受付19:00）",
+    mapUrl: "https://www.google.com/maps/embed?pb=...埋め込みURL...",
+  };
+
   return (
-    <>
-      {/* …他のセクション… */}
+    <div>
+      <Hero />
+      <Features />
+      <Menu />
+      <Pricing />
+      <Testimonials />
+      <ReservationDialog />
       <Access salon={salonData} />
-      {/* …Footerなど… */}
-    </>
+      <Footer />
+    </div>
   );
 }
