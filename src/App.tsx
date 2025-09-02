7953143aa2d6b78624620e4b0e58653b81b71878
osei224 <seikann0224@gmail.com>
2025-09-02 10:28:59 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@
 import React from "react";
 import "./index.css";
 import Access from "./components/Access";
-// 他のコンポーネント（Hero, Menu, Footer など）も必要なら import してね
 
 const salonData = {
   name: "HAIR & SPA Lumière",
   address: "大阪市〇〇区〇〇 1-2-3 ルミエールビル2F",
-  mapUrl: "https://www.google.com/maps/embed?pb=...（Googleマップ埋め込みURL）",
+  mapUrl:
+    "https://www.google.com/maps/embed?pb=...（ここを実際の埋め込みURLに）",
   phone: "06-1234-5678",
   hours: "10:00-20:00（最終受付19:00）",
 };
@@ -15,9 +15,9 @@ const salonData = {
 export default function App() {
   return (
     <>
-      {/* 他のセクション */}
+      {/* …他のセクション… */}
       <Access salon={salonData} />
-      {/* Footer など */}
+      {/* …Footerなど… */}
     </>
   );
 }
